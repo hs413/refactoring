2a467aa68d4507fa33a8eed79fbf66567d3150ab
rtg <rtg2541@gmail.com>
2024-11-17 15:05:53 +0900

    1장: totalAmount 변수 제거하기2

--- 1장 리팩터링 예시/index.js
+++ 1장 리팩터링 예시/index.js
@@ -8,28 +8,26 @@ function statement(invoice, plays) {
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
   }
 
-  let totalAmount = appleSauce();
-
-  result += `총액: ${usd(totalAmount)}\n`;
+  result += `총액: ${usd(totalAmount())}\n`;
   result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
 
   function totalVolumeCredits() {
-    let volumeCredits = 0;
+    let result = 0;
     for (let perf of invoice.performances) {
-      volumeCredits += volumeCreditsFor(perf);
+      result += volumeCreditsFor(perf);
     }
 
-    return volumeCredits;
+    return result;
   }
 
-  function appleSauce() {
-    let totalAmount = 0;
+  function totalAmount() {
+    let result = 0;
     for (let perf of invoice.performances) {
       // 청구 내역을 출력한다.
-      totalAmount += amountFor(perf);
+      result += amountFor(perf);
     }
-    return totalAmount;
+    return result;
   }
 }
 
