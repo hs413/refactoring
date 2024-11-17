94b273ba2c2fd3afbf262f709204ac5fe96a78aa
rtg <rtg2541@gmail.com>
2024-11-17 20:30:22 +0900

    1장: 단계 분리하기4

--- 1장 리팩터링 예시/index.js
+++ 1장 리팩터링 예시/index.js
@@ -10,42 +10,12 @@ function statement(invoice, plays) {
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
+    result.amount = amountFor(result);
     return result;
   }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
-  }
-}
-
-function renderPlainText(data, plays) {
-  let result = `청구 내역 (고객명: ${data.customer})\n`;
-
-  for (let perf of data.performances) {
-    result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
-  }
-
-  result += `총액: ${usd(totalAmount())}\n`;
-  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
-  return result;
-
-  function totalVolumeCredits() {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += volumeCreditsFor(perf);
-    }
-
-    return result;
-  }
-
-
-  function totalAmount() {
-    let result = 0;
-    for (let perf of data.performances) {
-      // 청구 내역을 출력한다.
-      result += amountFor(perf);
-    }
-    return result;
   }
 
   function amountFor(aPerformance) {
@@ -69,8 +39,36 @@ function renderPlainText(data, plays) {
     }
     return result;
   }
+}
 
+function renderPlainText(data, plays) {
+  let result = `청구 내역 (고객명: ${data.customer})\n`;
 
+  for (let perf of data.performances) {
+    result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
+  }
+
+  result += `총액: ${usd(totalAmount())}\n`;
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+  return result;
+
+  function totalVolumeCredits() {
+    let result = 0;
+    for (let perf of data.performances) {
+      result += volumeCreditsFor(perf);
+    }
+
+    return result;
+  }
+
+  function totalAmount() {
+    let result = 0;
+    for (let perf of data.performances) {
+      // 청구 내역을 출력한다.
+      result += perf.amount;
+    }
+    return result;
+  }
 
   function volumeCreditsFor(aPerformance) {
     let result = 0;
