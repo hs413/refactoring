123cb80aaccfd5ffabff178ec3175948f36e1b42
rtg <rtg2541@gmail.com>
2024-11-17 20:35:37 +0900

    1장: 단계 분리하기5

--- 1장 리팩터링 예시/index.js
+++ 1장 리팩터링 예시/index.js
@@ -5,12 +5,16 @@ function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer
   statementData.performances = invoice.performances.map(enrichPerformance)
+  statementData.totalAmount = totalAmount(statementData);
+  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
   return renderPlainText(statementData, plays);
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
     result.amount = amountFor(result);
+    result.volumeCredits = volumeCreditsFor(result);
+
     return result;
   }
 
@@ -39,36 +43,6 @@ function statement(invoice, plays) {
     }
     return result;
   }
-}
-
-function renderPlainText(data, plays) {
-  let result = `청구 내역 (고객명: ${data.customer})\n`;
-
-  for (let perf of data.performances) {
-    result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
-  }
-
-  result += `총액: ${usd(totalAmount())}\n`;
-  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
-  return result;
-
-  function totalVolumeCredits() {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += volumeCreditsFor(perf);
-    }
-
-    return result;
-  }
-
-  function totalAmount() {
-    let result = 0;
-    for (let perf of data.performances) {
-      // 청구 내역을 출력한다.
-      result += perf.amount;
-    }
-    return result;
-  }
 
   function volumeCreditsFor(aPerformance) {
     let result = 0;
@@ -78,6 +52,36 @@ function renderPlainText(data, plays) {
 
     return result;
   }
+
+  function totalAmount(data) {
+    let result = 0;
+    for (let perf of data.performances) {
+      // 청구 내역을 출력한다.
+      result += perf.amount;
+    }
+    return result;
+  }
+
+  function totalVolumeCredits(data) {
+    let result = 0;
+    for (let perf of data.performances) {
+      result += perf.volumeCredits;
+    }
+
+    return result;
+  }
+}
+
+function renderPlainText(data, plays) {
+  let result = `청구 내역 (고객명: ${data.customer})\n`;
+
+  for (let perf of data.performances) {
+    result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
+  }
+
+  result += `총액: ${usd(data.totalAmount)}\n`;
+  result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
+  return result;
 
   function usd(aNumber) {
     return new Intl.NumberFormat("en-US", {
