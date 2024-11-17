f45d9f03e98286083506412d01f2d64fffdae71b
rtg <rtg2541@gmail.com>
2024-11-17 14:36:19 +0900

    1장: format 변수 제거

--- 1장 리팩터링 예시/index.js
+++ 1장 리팩터링 예시/index.js
@@ -5,11 +5,6 @@ function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-  const format = new Intl.NumberFormat("en-US", {
-    style: "currency", currency: "USD",
-    minimumFractionDigits: 2
-  }).format;
-
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
 
@@ -56,6 +51,13 @@ function volumeCreditsFor(aPerformance) {
     result += Math.floor(aPerformance.audience / 5);
 
   return result;
+}
+
+function format(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency", currency: "USD",
+    minimumFractionDigits: 2
+  }).format(aNumber);
 }
 
 console.log(statement(invoice[0], plays));
