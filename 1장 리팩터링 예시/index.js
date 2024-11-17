8b4150902edefa66e5f2d4e2d57711d4bf798d28
rtg <rtg2541@gmail.com>
2024-11-17 21:08:17 +0900

    1장: 단계 분리하기6

--- 1장 리팩터링 예시/index.js
+++ 1장 리팩터링 예시/index.js
@@ -1,95 +1,30 @@
-const invoice = require('./invoices.json');
-const plays = require('./plays.json');
+import { statement, htmlStatement } from './statement.js';
 
-function statement(invoice, plays) {
-  const statementData = {};
-  statementData.customer = invoice.customer
-  statementData.performances = invoice.performances.map(enrichPerformance)
-  statementData.totalAmount = totalAmount(statementData);
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-  return renderPlainText(statementData, plays);
-
-  function enrichPerformance(aPerformance) {
-    const result = Object.assign({}, aPerformance);
-    result.play = playFor(result);
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
-
-    return result;
+const invoice = [
+  {
+    "customer": "BigCo",
+    "performances": [
+      {
+        "playID": "hamlet",
+        "audience": 55
+      },
+      {
+        "playID": "as-like",
+        "audience": 35
+      },
+      {
+        "playID": "othello",
+        "audience": 40
+      }
+    ]
   }
+]
 
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
-
-  function amountFor(aPerformance) {
-    let result = 0;
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 1000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-    }
-    return result;
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === aPerformance.play.type)
-      result += Math.floor(aPerformance.audience / 5);
-
-    return result;
-  }
-
-  function totalAmount(data) {
-    let result = 0;
-    for (let perf of data.performances) {
-      // 청구 내역을 출력한다.
-      result += perf.amount;
-    }
-    return result;
-  }
-
-  function totalVolumeCredits(data) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.volumeCredits;
-    }
-
-    return result;
-  }
+const plays = {
+  "hamlet": { "name":"Hamlet", "type": "tragedy" },
+  "as-like": { "name":"As You Like It", "type": "comedy" },
+  "othello": { "name":"Othello", "type": "tragedy" }
 }
-
-function renderPlainText(data, plays) {
-  let result = `청구 내역 (고객명: ${data.customer})\n`;
-
-  for (let perf of data.performances) {
-    result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
-  }
-
-  result += `총액: ${usd(data.totalAmount)}\n`;
-  result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
-  return result;
-
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency", currency: "USD",
-      minimumFractionDigits: 2
-    }).format(aNumber / 100);
-  }
-}
-
 
 console.log(statement(invoice[0], plays));
+console.log(htmlStatement(invoice[0], plays));
