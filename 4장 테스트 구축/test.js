1c6313cf385b3377643832dff890446f2302b84f
rtg <rtg2541@gmail.com>
2024-11-21 22:30:25 +0900

    4장 테스트: 두 번째 테스트

--- 4장 테스트 구축/test.js
+++ 4장 테스트 구축/test.js
@@ -5,6 +5,15 @@ import {Province, sampleProvinceData, Producer} from "./Province.js";
 describe('province', function() {
   it('shortfall', function() {
     const asia = new Province(sampleProvinceData());
+
+    // assert 또는 expect를 사용할 수 있다
     assert.equal(asia.shortfall, 5);
+    expect(asia.shortfall).equal(5);
+  })
+
+  it('profit', function() {
+    const asia = new Province(sampleProvinceData());
+    expect(asia.profit).equal(230);
   })
 })
+
