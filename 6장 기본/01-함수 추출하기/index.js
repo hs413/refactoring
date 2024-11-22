7f6a8f673b43b060349099cccce7e125b875c522
rtg <rtg2541@gmail.com>
2024-11-22 20:59:57 +0900

    6장 기본 리팩터링: 지역 변수 값 변경

--- 6장 기본/01-함수 추출하기/index.js
+++ 6장 기본/01-함수 추출하기/index.js
@@ -1,16 +1,16 @@
 function printOwing(invoice) {
-  let outstanding = 0;
-
   printBanner()
-
-  // 미해결 채무(outstanding)를 계산한다
-  for (const o of invoice.orders) {
-    outstanding += o.amount
-  }
-
+  const outstanding = calculateOutstanding()
   recordDueDate(invoice)
-
   printDetails(invoice, outstanding)
+}
+
+function calculateOutstanding(invoice) {
+  let result = 0;
+  for (const o of invoice.orders) {
+    result += o.amount
+  }
+  return result
 }
 
 function printBanner() {
@@ -29,3 +29,4 @@ function recordDueDate(invoice) {
   const today = Clock.today;
   invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
 }
+
