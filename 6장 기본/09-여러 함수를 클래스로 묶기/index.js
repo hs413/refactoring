d2e78f8cf1bb0de757243884a70f5c92dc02aea2
rtg <rtg2541@gmail.com>
2024-11-23 16:46:47 +0900

    6장 기본 리팩터링: 함수 클래스로 묶기 클래스 활용

--- 6장 기본/09-여러 함수를 클래스로 묶기/index.js
+++ 6장 기본/09-여러 함수를 클래스로 묶기/index.js
@@ -12,15 +12,38 @@ function client2 () {
 }
 
 function client3 () {
-  const aReading = acquireReading();
-  const basecChargeAmoun  = calculateBaseCharge();
-
-  function calculateBaseCharge(aReading) {
-    return  baseRate(aReading.month, aReading.year) * aReading.quantity
-  }
+  const rawReading = acquireReading();
+  const aReading = new Reading(rawReading)
+  const basicChargeAmount  = aReading.baseCharge;
 }
 
+class Reading {
+  constructor(data) {
+    this._customer = data.customer;
+    this._quantity = data.quantity;
+    this._month = data.month;
+    this._year = data.year;
+  }
 
+  get customer() {
+    return this._customer
+  }
+
+  get quantity() {
+    return this._quantity;
+  }
+
+  get month() {
+    return this._month;
+  }
+
+  get year() {
+    return this._year;
+  }
+  get baseCharge() {
+    return baseRate(this.month, this.year) * this.quantity
+  }
+}
 
 
 
