bc58804c08f2f8928886be6620d1dac8355eef46
rtg <rtg2541@gmail.com>
2024-11-24 10:59:53 +0900

    6장 기본 리팩터링: 단계 쪼개기 첫 번째 단계를 처리하는 함수

--- 6장 기본/11-단계 쪼개기/index.js
+++ 6장 기본/11-단계 쪼개기/index.js
@@ -1,15 +1,16 @@
 function priceOrder(product, quantity, shippingMethod) {
+  const priceData = calculatePricingData(product, quantity);
+  return applyShipping(priceData, shippingMethod);
+}
+
+function calculatePricingData(product, quantity) {
   const basePrice = product.basePrice * quantity;
   const discount = Math.max(quantity - product.discountThreshold, 0) * product.basePrice * product.discountRate;
-  const priceData = { basePrice, quantity, discount };
-  const price = applyShipping(priceData, shippingMethod);
-  return price;
+  return { basePrice, quantity, discount };
 }
 
 function applyShipping(priceData, shippingMethod) {
   const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold) ? shippingMethod.discountThreshold : shippingMethod.feePerCase;
   const shippingCost = priceData.quantity * shippingPerCase;
-  const price = priceData.basePrice - priceData.discount + shippingCost;
-
-  return price;
+  return priceData.basePrice - priceData.discount + shippingCost;
 }
