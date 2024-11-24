ac84f74341ff10249d58b88656f9bbcf10a6eb17
rtg <rtg2541@gmail.com>
2024-11-24 10:57:40 +0900

    6장 기본 리팩터링: 단계 쪼개기 중간 데이터 구조

--- 6장 기본/11-단계 쪼개기/index.js
+++ 6장 기본/11-단계 쪼개기/index.js
@@ -1,14 +1,15 @@
 function priceOrder(product, quantity, shippingMethod) {
   const basePrice = product.basePrice * quantity;
   const discount = Math.max(quantity - product.discountThreshold, 0) * product.basePrice * product.discountRate;
-  const price = applyShipping(basePrice, shippingMethod, quantity, discount);
+  const priceData = { basePrice, quantity, discount };
+  const price = applyShipping(priceData, shippingMethod);
   return price;
 }
 
-function applyShipping(basePrice, shippingMethod, quantity, discount) {
-  const shippingPerCase = (basePrice > shippingMethod.discountThreshold) ? shippingMethod.discountThreshold : shippingMethod.feePerCase;
-  const shippingCost = quantity * shippingPerCase;
-  const price = basePrice - discount + shippingCost;
+function applyShipping(priceData, shippingMethod) {
+  const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold) ? shippingMethod.discountThreshold : shippingMethod.feePerCase;
+  const shippingCost = priceData.quantity * shippingPerCase;
+  const price = priceData.basePrice - priceData.discount + shippingCost;
 
   return price;
 }
