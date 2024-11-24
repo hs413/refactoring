e00ac4cd27c6c3ecc6274491a01030d8f58a9080
rtg <rtg2541@gmail.com>
2024-11-24 11:19:24 +0900

    7장 캡슐화: 간단한 레코드 캡슐화2

--- 7장 캡슐화/01-레코드 캡슐화/simple.js
+++ 7장 캡슐화/01-레코드 캡슐화/simple.js
@@ -1,5 +1,29 @@
+// 기본 코드
 const organization = { name: "aaa", country: "BG" };
 
 result += `<h1>${organization.name}</h1>`; // 읽기
 organization.name = newName // 쓰기
+
+
+// 변수 캡슐화
+function getRawDataOfOrganization() {
+  return organization;
+}
+result += `<h1>${getRawDataOfOrganization().name}</h1>`; // 읽기
+getRawDataOfOrganization().name = newName // 쓰기
+
+// 클래스로 변경
+class Organization {
+  constructor(data) {
+    this._data = data;
+  }
+}
+
+const organization = new Organization({ name: "aaa", country: "BG" });
+function getRawDataOfOrganization() {
+  return organization._data;
+}
+function getOrganization() {
+  return organization
+}
 
