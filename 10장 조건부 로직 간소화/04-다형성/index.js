cbc62975afa79e4520271a15c264f6d775b108f7
rtg <rtg2541@gmail.com>
2024-11-26 22:54:19 +0900

    10장 조건부 간소화: 다형성 예시 서브 클래스를 이용하여 다형성 활용

--- 10장 조건부 로직 간소화/04-다형성/index.js
+++ 10장 조건부 로직 간소화/04-다형성/index.js
@@ -7,11 +7,24 @@ function speeds(birds) {
 }
 
 function plumage(bird) {
-  return new Bird(bird).plumage;
+  return createBird(bird).plumage;
 }
 
 function airSpeedVelocity(bird) {
-  return new Bird(bird).airSpeedVelocity;
+  return createBird(bird).airSpeedVelocity;
+}
+
+function createBird(bird) {
+  switch (bird.type) {
+    case '유럽 제비':
+      return
+    case '아프리카 제비':
+      return ;
+    case '노르웨이 파랑 앵무':
+      return ;
+    default:
+      return new Bird(bird);
+  }
 }
 
 class Bird {
@@ -20,29 +33,41 @@ class Bird {
   }
 
   get plumage() {
-    switch (this.type) {
-      case '유럽 제비':
-        return '보통이다'
-      case '아프리카 제비':
-        return (this.numberOfCoconuts > 2) ? '지쳤다' : '보통이다';
-      case '노르웨이 파랑 앵무':
-        return (this.voltage > 100) ? '그을렸다' : '예쁘다';
-      default:
-        return '알 수 없다';
-    }
+    return '알 수 없다';
   }
 
   get airSpeedVelocity() {
-    switch (this.type) {
-      case '유럽 제비':
-        return 35;
-      case '아프리카 제비':
-        return 40 - 2 * this.numberOfCoconuts;
-      case '노르웨이 파랑 앵무':
-        return (this.isNailed) ? 0 : 10 + this.voltage / 10;
-      default:
-        return null;
-    }
+    return null;
+  }
+}
+
+class EuropeanSwallow extends Bird {
+  get plumage() {
+    return '보통이다'
+  }
+
+  get airSpeedVelocity() {
+    return 35;
+  }
+}
+
+class AfricanSwallow extends Bird {
+  get plumage() {
+    return (this.numberOfCoconuts > 2) ? '지쳤다' : '보통이다';
+  }
+
+  get airSpeedVelocity() {
+    return 40 - 2 * this.numberOfCoconuts;
+  }
+}
+
+class NorwegianBlueParrot extends Bird {
+  get plumage() {
+    return (this.voltage > 100) ? '그을렸다' : '예쁘다';
+  }
+
+  get airSpeedVelocity() {
+    return (this.isNailed) ? 0 : 10 + this.voltage / 10;
   }
 }
 
